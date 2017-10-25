44b6da37d30c0392230b0d99c9c96663a2e9e5ae
Vangelis <kelomaniack@hotmail.com>
2017-10-25 21:23:13 +0200

    local copies

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CapstoneDemoapp
+module Myapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -28,6 +28,23 @@ module CapstoneDemoapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    Mongoid.load!('./config/mongoid.yml')
+    #which default ORM are we using with scaffold
+    #add  --orm mongoid, or active_record 
+    #    to rails generate cmd line to be specific
+    config.generators {|g| g.orm :active_record}
+    #config.generators {|g| g.orm :mongoid}
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+
+        resource '/api/*', 
+          :headers => :any, 
+          :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
