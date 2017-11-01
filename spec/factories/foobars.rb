0ff5d4759de3a10cbac3405b17d2b28729094396
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-11-01 18:01:46 +0100

    FactoryGirl Factories

--- spec/factories/foobars.rb
+++ spec/factories/foobars.rb
@@ -3,7 +3,21 @@ FactoryGirl.define do
     name "test"
   end
 
-  factory :foo, :parent => :foo_fixed do
-  	name "test"
+  factory :foo_sequence, class: 'Foo' do
+  	sequence(:name) {|n| "test#{n}" }
+  end
+
+  factory :foo_names, class: 'Foo' do
+  	sequence(:name) {|n| ["larry", "moe", "curly"][n%3]}
+  end
+
+  factory :foo_transient, class: 'Foo' do
+  	transient do
+  		male true
+  	end
+
+  	after(:build) do |object, props|
+  		object.name  = props.male ? "Mr Test" : "Ms Test"
+  	end
   end
 end
