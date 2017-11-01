e84a3e458d658a578584896534f0001727995738
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-11-01 12:17:03 +0100

    Spec Anatomy Part 2: Thinning Examples

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 describe Foo, type: :model do
 	context "created Foo" do  
-		let(:foo) { Foo.create(:name=>"test") }
+	let(:foo) { Foo.create(:name=>"test") }
 		
-		it "will be persisted, have a name, and be found" do
-			expect(foo).to be_persisted
-			expect(foo.name).to eq("test")
-			expect(Foo.find(foo.id)).to_not be_nil
-		end
+		it { expect(foo).to be_persisted }
+		it { expect(foo.name).to eq("test") }
+		it { expect(Foo.find(foo.id)).to_not be_nil }
 	end
+	context "created Foo" do
+		subject { Foo.create(:name=>"test") }
+		     
+		 it { is_expected.to be_persisted }
+		 it { expect(subject.name).to eq("test") }
+		 it { expect(Foo.find(subject.id)).to_not be_nil }
+	end     
 end
