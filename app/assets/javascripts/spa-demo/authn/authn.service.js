f1818553f2a3cfc959328baea1f72853e9fb69c0
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-11-13 12:10:59 +0100

    UI: Navbar and Authn Session

--- app/assets/javascripts/spa-demo/authn/authn.service.js
+++ app/assets/javascripts/spa-demo/authn/authn.service.js
@@ -2,8 +2,8 @@
     'use strict';
 
     angular
-        .module('spa-demo.authn')
-        .service('spa-demo.authn.Authn', Authn);
+    .module('spa-demo.authn')
+    .service('spa-demo.authn.Authn', Authn);
 
     Authn.$inject = ["$auth"];
 
@@ -11,6 +11,10 @@
     function Authn($auth) {
         var service = this;
         service.signup = signup;
+        service.user = null;
+        service.isAuthenticated = isAuthenticated;
+        service.getCurrentUser = getCurrentUser;
+        service.getCurrentUserName = getCurrentUserName;
 
         return;
         ////////////////
@@ -18,5 +22,18 @@
         function signup(registration) {
         	return  $auth.submitRegistration(registration);
         }
-    }
+
+        function isAuthenticated() {
+          return service.user!=null && service.user["uid"]!=null;
+        }
+        function getCurrentUserName() {
+          return service.user!=null ? service.user.name : null;
+        }
+        function getCurrentUserId() {
+          return service.user!=null ? service.user.id : null;
+        }
+        function getCurrentUser() {
+          return service.user;
+        }
+  }
 })();
