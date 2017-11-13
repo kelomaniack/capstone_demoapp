5721c648f2309a832c253de037957b14a5394264
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-11-13 11:36:05 +0100

    UI: Signup Error Handling and Form Feedback

--- app/assets/javascripts/spa-demo/authn/signup/signup.component.js
+++ app/assets/javascripts/spa-demo/authn/signup/signup.component.js
@@ -25,6 +25,7 @@
     	///////////////
     	function signup() {
     		console.log("signup...");
+            $scope.signup_form.$setPristine();
     		Authn.signup(vm.signupForm).then(
     			function(response) {
     				vm.id = response.data.data.id;
@@ -32,6 +33,7 @@
     				$state.go("home");
     			},
     			function(response){
+                    vm.signupForm["errors"]=response.data.errors;
     				console.log("signup failure", response, vm);
     			}
     			);
